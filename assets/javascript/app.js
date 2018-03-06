3ef4f94603de0acb1d626670579ad0c09b493202
Juan Fox <foxj0875@gmail.com>
2018-03-06 15:29:58 -0500

    buttons are generated from an array and logging their data attributes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,18 @@
-console.log('ready');
+var cartoonCharacters = [
+  'bugs bunny',
+  'roadrunner',
+  'sponge bob'
+];
+
+cartoonCharacters.forEach(function(item) {
+  var button = $('<button>');
+
+  button.attr('data-name', item);
+  button.addClass('btn btn-primary m-5');
+  button.text(item);
+  $('body').append(button);
+
+  button.click(function() {
+    console.log($(this).attr('data-name'));
+  });
+});
