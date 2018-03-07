e16d8399c6969f026f6f4f74e9cf3aa24a1671fb
Juan Fox <foxj0875@gmail.com>
2018-03-07 15:43:14 -0500

    gif buttons displaying gifs now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ var cartoonCharacters = [
   'roadrunner',
   'sponge bob'
 ];
-var url = 'https://api.giphy.com/v1/gifs/search?api_key=rgsRNl9LWGRa8eUxe0kdPR44lKJ23ETI&q=&limit=25&offset=0&rating=G&lang=en&q=bugs+bunny';
+
 
 cartoonCharacters.forEach(function(item) {
   var button = $('<button>');
@@ -11,7 +11,7 @@ cartoonCharacters.forEach(function(item) {
   button.attr('data-name', item);
   button.addClass('btn btn-primary m-5 gif');
   button.text(item);
-  $('main').append(button);
+  $('#buttons').append(button);
 
   
 });
@@ -24,17 +24,35 @@ $('button[type=submit]').click(function(e) {
   button.attr('data-name', val);
   button.addClass('btn btn-primary m-5 gif');
   button.text(val);
-  $('main').append(button);
+  $('#buttons').append(button);
   val = '';
 });
 
 $('main').on('click', 'button.gif', function() {
   console.log($(this).attr('data-name'));
+  $('#content').empty();
+  var character = $(this).attr('data-name');
+  var url = 'https://api.giphy.com/v1/gifs/search?api_key=rgsRNl9LWGRa8eUxe0kdPR44lKJ23ETI&q=&limit=10&offset=0&lang=en&q=' + character + ';'
   $.ajax({
     url: url,
     method: 'GET'
   })
   .then(function(response) {
     console.log(response);
+
+    var gifArray = response.data;
+
+    gifArray.forEach(function(item) {
+      var cartoon = $('<div>');
+      var html = '<img src="' + item.images.fixed_height.url + '"/>';
+
+      cartoon.html(html);
+      
+      $('#content').append(html);
+    });
+
+    
+
+    
   });
 });
