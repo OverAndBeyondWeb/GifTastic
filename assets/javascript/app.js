02b41ce4765c82e1b6b4ce064f8450f420fa1622
Juan Fox <foxj0875@gmail.com>
2018-03-07 15:07:36 -0500

    add cartoon character input and submit button in html now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,7 @@ var cartoonCharacters = [
   'roadrunner',
   'sponge bob'
 ];
+var url = 'https://api.giphy.com/v1/gifs/search?api_key=rgsRNl9LWGRa8eUxe0kdPR44lKJ23ETI&q=&limit=25&offset=0&rating=G&lang=en&q=bugs+bunny';
 
 cartoonCharacters.forEach(function(item) {
   var button = $('<button>');
@@ -14,5 +15,12 @@ cartoonCharacters.forEach(function(item) {
 
   button.click(function() {
     console.log($(this).attr('data-name'));
+    $.ajax({
+      url: url,
+      method: 'GET'
+    })
+    .then(function(response) {
+      console.log(response);
+    });
   });
 });
